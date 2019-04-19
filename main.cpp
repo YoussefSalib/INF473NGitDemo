22497733918bc51d6c5fe6a81c1228f28ce192cc
Youssef Salib <youssef.salib@polytechnique.edu>
2019-04-19 11:54:14 +0200

    cc

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main(){
       std::cout << "Hello World" << std::endl;
             std::cout << "HEC c'est vraiment super" << std::endl;
-            std::cout << "Prout" << std::endl;
+            std::cout << "Prout Prout" << std::endl;
 
 
 return 0;
